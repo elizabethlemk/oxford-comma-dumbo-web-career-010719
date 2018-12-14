591cd1812a5f10af375f092eea1f936c34bf8f92
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-12-14 20:53:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+  puts array[0]
+elsif array.length == 2
+  puts "#{array[0] and array[1]}"
+elsif array.length == 3
+  puts "#{array[0..2].join(", ")} and #{array.last}"
+else
+  puts "#{array[0..-2].join(", ")} and #{array.last}"
+  end
 end
